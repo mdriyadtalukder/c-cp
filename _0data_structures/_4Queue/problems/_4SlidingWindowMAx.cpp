0edbebb34b954c275289b75a7448cd3f0339da25
mdriyadtalukder <riyadtalukder67@gmail.com>
2026-03-02 19:10:10 +0600

    added

---
+++ _0data_structures/_4Queue/problems/_4SlidingWindowMAx.cpp
@@ -0,0 +1,39 @@
+
+
+// class Solution
+// {
+// public:
+//     vector<int> maxSlidingWindow(vector<int> &nums, int k)
+//     {
+//         deque<int> dq;
+//         vector<int> res;
+//         // 1st wind
+//         // dequeue e element push krbo and pop krbo from back jdi current elem boro hy.
+//         for (int i = 0; i < k; i++)
+//         {
+//             while (dq.size() > 0 && nums[dq.back()] <= nums[i])
+//             {
+//                 dq.pop_back();
+//             }
+//             dq.push_back(i);
+//         }
+//         for (int i = k; i < nums.size(); i++)
+//         {
+//             res.push_back(nums[dq.front()]); // push result
+//             // remove not part of curr window..remove krbo shamne theke
+//             while (dq.size() > 0 && dq.front() <= i - k)
+//             {
+//                 dq.pop_front();
+//             }
+
+//             // remove the smaller vals
+//             while (dq.size() > 0 && nums[dq.back()] <= nums[i])
+//             {
+//                 dq.pop_back();
+//             }
+//             dq.push_back(i);
+//         }
+//         res.push_back(nums[dq.front()]);
+//         return res;
+//     }
+// };

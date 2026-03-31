48eb2401ae66aa3e3f1e0e2ed5f7eb6dbbc14445
mdriyadtalukder <riyadtalukder67@gmail.com>
2026-03-31 19:09:58 +0600

    added

--- 19Practice.cpp
+++ 19Practice.cpp
@@ -6,66 +6,21 @@ int main()
     cin >> x;
     while (x--)
     {
-        int n;
-        bool b = false;
-        cin >> n;
-        string s;
-        cin >> s;
-        stack<char> st;
-        for (auto &l : s)
+        int y, z;
+        string a, b, c;
+        cin >> y >> a >> z >> b >> c;
+
+        for (int i = 0; i < c.length(); i++)
         {
-            l = tolower(l);
-        }
-        int m = 0, e = 0, o = 0, w = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (s[i] == 'm')
+            if (c[i] == 'D')
             {
-                m++;
+                a.push_back(b[i]);
             }
-            if (s[i] == 'e')
+            else
             {
-                e++;
-            }
-            if (s[i] == 'o')
-            {
-                o++;
-            }
-            if (s[i] == 'w')
-            {
-                w++;
+                a.insert(0, string(1, b[i]));
             }
         }
-        if (m > 0 && e > 0 && o > 0 && w > 0)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                if (!st.empty())
-                {
-                    if ((st.top() == 'm' && (s[i] == 'm' || s[i] == 'e')) || (st.top() == 'e' && (s[i] == 'e' || s[i] == 'o')) || (st.top() == 'o' && (s[i] == 'o' || s[i] == 'w')) || (st.top() == 'w' && (s[i] == 'w')))
-                    {
-                        b = false;
-                    }
-                    else
-                    {
-                        b = true;
-                        break;
-                    }
-                }
-                st.push(s[i]);
-            }
-        }
-        else
-        {
-            b = true;
-        }
-        if (b)
-        {
-            cout << "NO" << endl;
-        }
-        else
-        {
-            cout << "YES" << endl;
-        }
+        cout << a << endl;
     }
 }
--- _8data_structures/_1DynamicArray/problems/_6productOfArrayExceptSelf.cpp
+++ _8data_structures/_1DynamicArray/problems/_6productOfArrayExceptSelf.cpp
@@ -33,3 +33,40 @@ int main()
 
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     vector<int> nums = {1, 2, 3, 4};
+//     int n = nums.size();
+
+//     vector<int> prefix(n, 1), suffix(n, 1), ans(n);
+
+//     // prefix array
+//     for (int i = 1; i < n; i++)
+//     {
+//         prefix[i] = prefix[i - 1] * nums[i - 1];
+//     }
+
+//     // suffix array
+//     for (int i = n - 2; i >= 0; i--)
+//     {
+//         suffix[i] = suffix[i + 1] * nums[i + 1];
+//     }
+
+//     // result
+//     for (int i = 0; i < n; i++)
+//     {
+//         ans[i] = prefix[i] * suffix[i];
+//     }
+
+//     // print result
+//     for (int x : ans)
+//     {
+//         cout << x << " ";
+//     }
+
+//     return 0;
+// }

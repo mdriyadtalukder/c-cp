e2064feed5e6da162fc7455d86523efd80606e51
mdriyadtalukder <riyadtalukder67@gmail.com>
2026-04-06 18:48:10 +0600

    added

--- 19Practice.cpp
+++ 19Practice.cpp
@@ -3,4 +3,33 @@ using namespace std;
 
 int main()
 {
+    int a, b;
+    cin >> a >> b;
+    char ar[a][b];
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            cin >> ar[i][j];
+        }
+    }
+    int c = 0;
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            if (ar[i][j] == 'C' || ar[i][j] == 'M' || ar[i][j] == 'Y')
+            {
+                c++;
+            }
+        }
+    }
+    if (c == 0)
+    {
+        cout << "#Black&White" << endl;
+    }
+    else
+    {
+        cout << "#Color" << endl;
+    }
 }
